9bd8f68a001319489503768bc01a91a845421b02
Dust Of Snow <new.to.world2003@gmail.com>
2022-04-06 00:16:36 +0530

    Played with the App a little Bit

--- react-projects/apps/pokedex/src/app/app.tsx
+++ react-projects/apps/pokedex/src/app/app.tsx
@@ -2,13 +2,31 @@
 import React from 'react';
 import NxWelcome from './nx-welcome';
 
-export const App:React.FC = () => {
+const DUMMY_DATA = [
+  {
+    id: 'FEST45',
+    name: 'Apurba Ghosh',
+    Role: 'Frontend Dev/ FullStack Developer',
+  },
+];
+
+export const App: React.FC = () => {
   return (
     <>
       <NxWelcome title="pokedex" />
+      {DUMMY_DATA.map(data => JSON.stringify(data))}
+      {DUMMY_DATA.map((data) => {
+        return (
+          <div>
+            <h3>Name: {data.name}</h3>
+            <h2>ID: {data.id}</h2>
+            <h1>Role: {data.Role}</h1>
+          </div>
+        );
+      })}
       <div />
     </>
   );
-}
+};
 
 export default App;
